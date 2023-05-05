d6f59317a22dd9eec94fcb6bbbe2d60631d1b311
Odysseas Efremidis <oefremidis@athtech.gr>
2023-05-05 12:19:42 +0300

    Final Commit

--- arrays.js
+++ arrays.js
@@ -112,5 +112,7 @@ const colors = ['Red', 'Green', 'Blue'];
 // const greaterThanFive = numbers.filter((num) => num > 5);
 // console.log(greaterThanFive)
 
-const numbers = [1,2,3,4,5,6,7,8,9];
-numbers.filter((num) => num > 5).forEach(value => console.log(value));
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+numbers
+    .filter((num) => num > 5)
+    .forEach(value => console.log(value));
